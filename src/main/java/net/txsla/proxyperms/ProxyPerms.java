fd0f83f11b4b8db56c98c5af5a23cab27f244773
_Txsla <104021913+Tesla6426@users.noreply.github.com>
2025-04-05 12:54:29 -0700

    created?

---
+++ src/main/java/net/txsla/proxyperms/ProxyPerms.java
@@ -0,0 +1,22 @@
+package net.txsla.proxyperms;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "proxyperms",
+        name = "ProxyPerms",
+        version = "1.1.1"
+)
+public class ProxyPerms {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
